6542deaab08b69b0940349481a7625fbea3fa02f
George Bockari <bitwhys@gmail.com>
2022-09-20 19:14:27 -0400

    feat(countDuplicateCharacters): implement problem using HashMap

--- src/main/java/com/bitwhys/codingProblems/stringsNumbersAndMath/StringsNumbersAndMath.java
+++ src/main/java/com/bitwhys/codingProblems/stringsNumbersAndMath/StringsNumbersAndMath.java
@@ -1,8 +1,30 @@
 package com.bitwhys.codingProblems.stringsNumbersAndMath;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
 public class StringsNumbersAndMath {
 
-  public int countDuplicateCharacters(String str) {
-    return 0;
+  public static void main(String[] args) {
+    Map<Character, Integer> characterIntegerMap = countDuplicateCharacters("George");
+    System.out.println(characterIntegerMap.toString());
+  }
+
+  public static Map<Character, Integer> countDuplicateCharacters(@NotNull String str) {
+    /*
+     * TODO: Write a program that counts duplicate characters from a given string
+     */
+    Map<Character, Integer> characterOccurrenceMap = new HashMap<Character, Integer>();
+    for (int index = 0; index < str.length(); ++index) {
+      Character currentCharacter = str.charAt(index);
+      if (!characterOccurrenceMap.containsKey(currentCharacter)) {
+        characterOccurrenceMap.put(currentCharacter, 1);
+      } else {
+        Integer characterCount = characterOccurrenceMap.get(currentCharacter);
+        characterOccurrenceMap.put(currentCharacter,++characterCount);
+      }
+    }
+    return characterOccurrenceMap;
   }
 }
